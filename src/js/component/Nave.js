9e27852c55acfaff6f41f9f1897ccc8ca7d1976e
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-06-17 01:28:31 +0000

    create ship component and test functionality

---
+++ src/js/component/Nave.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import rigoImage from "../../img/rigo-baby.jpg";
+
+export const Nave = (props) => {
+    
+ 
+    
+	return (
+		
+        <div className="card" style={{width: "18rem"}}>
+        <img src={rigoImage} className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">Nave Informacion</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      
+      
+	);
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useEffect, useState, useContext} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Nave } from "../component/Nave";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
+export const Home = () =>{
+    const [starships, setStarships] = useState  ([]);
+	const { store, actions } = useContext(Context);
+
+
+	useEffect(()=> {
+        console.log('se cargo el homeroo')
+		fetch('https://www.swapi.tech/api/starships')
+		.then( (response)=> response.json())
+		.then( (data)=> setStarships(data.results))
+      }, [])
+      
+
+	
+	return(
+	(
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>Home!</h1>
+		<h1>Naves Desde la API</h1>
+			
+
+
+{starships.map( (item)=> <Nave key={item.uid} title = {item.name}/>)}
+
+
+
 	</div>
+	)
 );
+}
